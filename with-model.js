7a9dc1d3a775c9add86ddbdf3b9925fc76195996
Jonathan Martin <me@nybblr.com>
2017-06-27 15:28:59 -0500

    Initial.

---
+++ with-model.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { compose, withState } from 'recompose';
+
+let fetchData = model => BaseComponent =>
+  class FetchData extends Component {
+    async componentWillMount() {
+      this.props.setData(await model(this.props))
+    }
+
+    render() {
+      return <BaseComponent {...this.props} />
+    }
+  }
+
+export default (model, initial) =>
+  compose(
+    withState('data', 'setData', initial),
+    fetchData(model)
+  )
